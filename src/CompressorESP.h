f8b8b05891281be3348e88498a201ba011a2f516
Christopher Lee <chris@chriselee.net>
2023-08-30 18:34:50 -0400

    begun creating class

---
+++ src/CompressorESP.h
@@ -0,0 +1,37 @@
+//CompressorESP.h
+
+/*This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "Arduino.h"
+#ifndef CompressorESP_h
+#define CompressorESP_h
+
+class Compressor {
+    private:
+    uint8_t contactor_pin;
+    uint8_t unloader_pin;
+    uint8_t tank_drain_pin;
+    bool ext_tank_drain = false;
+    int set_high_pressure;
+    int set_low_pressure;
+    int set_max_temp;
+    int set_min_temp;
+
+    public:
+    Compressor();
+};
+
+#endif
