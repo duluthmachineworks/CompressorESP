433e2d3c7526b24e0e9e3decb9778c3385336ba2
cleeUC <68129277+cleeUC@users.noreply.github.com>
2023-12-10 15:28:52 -0500

    added main example

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
 #include <CompressorESP.h>
 
-#define RELAY_PIN_1 21
-#define RELAY_PIN_2 19
-#define RELAY_PIN_3 18
-#define RELAY_PIN_4 5
+//Relay pins for LILYGO T-Relay Board
+#define RELAY_PIN_1 21 //Pump 1 contactor
+#define RELAY_PIN_2 19 //Pump 1 unloader
+#define RELAY_PIN_3 18 //Pump 2 contactor
+#define RELAY_PIN_4 5 //Pump 2 unloader
 #define LED_PIN     25
 
+Compressor pump1(RELAY_PIN_1, RELAY_PIN_2);
+Compressor pump2(RELAY_PIN_3, RELAY_PIN_4);
 
-void setup();
 
-void loop();
+void setup() {
+    pump1.begin();
+    pump2.begin();
+}
+
+void loop() {
+    pump1.unloadPump();
+    delay(500);
+    pump2.unloadPump();
+    delay(500);
+}
